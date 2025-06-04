310155e735407e8cdd8074c4c4a0eeb153326b28
Luc Fabresse <luc.fabresse@imt-nord-europe.fr>
2025-06-04 16:46:02 +0200

    ROS2 port

---
+++ launch/lidar_idec_se2l.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    # Declare launch arguments
+    sensor_ip = DeclareLaunchArgument("sensor_ip", default_value="10.0.0.5")
+
+    # LiDAR node
+    urg_node = Node(
+        package="urg_node",
+        executable="urg_node_driver",
+        name="urg_node_driver",
+        parameters=[
+            {"ip_address": LaunchConfiguration("sensor_ip")}
+        ]
+    )
+
+    # Static transform publisher
+    # static_transform_publisher = Node(
+    #     package="tf2_ros",
+    #     executable="static_transform_publisher",
+    #     name="laser_broadcaster",
+    #     arguments=["0.04", "0", "0", "0", "0", "0", "base_link", "laser"]
+    # )
+
+    return LaunchDescription([
+        urg_node,
+        # static_transform_publisher
+    ])
+
---
+++ launch/starter_kit.launch.py
@@ -0,0 +1,73 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Declare launch arguments
+    launch_args = [
+        DeclareLaunchArgument("master_ip", default_value="10.0.0.1"),
+        DeclareLaunchArgument("lidar_sensor", default_value="idec_se2l"),
+        DeclareLaunchArgument("lidar_sensor_ip", default_value="10.0.0.5"),
+        DeclareLaunchArgument("scan_prefix", default_value="laser_1"),
+        DeclareLaunchArgument("scan_topic", default_value=["/", LaunchConfiguration("scan_prefix"), "/scan"]),
+        DeclareLaunchArgument("scan_frame_id", default_value=LaunchConfiguration("scan_prefix")),
+        DeclareLaunchArgument("odom_frame_id", default_value="odom"),
+        DeclareLaunchArgument("base_frame_id", default_value="base_link"),
+        DeclareLaunchArgument("global_frame_id", default_value="map"),
+        DeclareLaunchArgument("baseline_m", default_value="0.485"),
+        DeclareLaunchArgument("joy_config", default_value="xbox"),
+        DeclareLaunchArgument("enable_cmd_vel_mux", default_value="true"),
+        DeclareLaunchArgument("enable_laser_filter", default_value="true"),
+        DeclareLaunchArgument("enable_joystick", default_value="true"),
+        DeclareLaunchArgument("enable_websocket", default_value="true"),
+        DeclareLaunchArgument("nav_mode", default_value="mapping"),
+        DeclareLaunchArgument("nav_algo", default_value="hector"),
+        DeclareLaunchArgument("nav_localization_static_map", default_value=os.path.join(os.getenv("ROS_PACKAGE_PATH", ""), "swd_starter_kit_bringup/assets/map_box2_v2.yaml")),
+    ]
+
+    # Include differential drive controller
+    diff_drive_controller_dir = get_package_share_directory('swd_ros2_controllers')
+    diff_drive_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(diff_drive_controller_dir + "/launch/swd_diff_drive_controller.launch.py"),
+        launch_arguments={
+            "baseline_m": LaunchConfiguration("baseline_m"),
+            "base_frame": LaunchConfiguration("base_frame_id"),
+            "odom_frame": LaunchConfiguration("odom_frame_id"),
+        }.items()
+    )
+
+    # Include LiDAR launch file
+    bringup_launch_dir = get_package_share_directory('swd_starter_kit_bringup')
+    lidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(bringup_launch_dir + "/launch/lidar_idec_se2l.launch.py"),
+        # PythonLaunchDescriptionSource(bringup_launch_dir + "/launch/lidar_" + LaunchConfiguration("lidar_sensor") + ".launch.py"),
+        launch_arguments={
+            "sensor_ip": LaunchConfiguration("lidar_sensor_ip")
+        }.items()
+    )
+
+    # Joystick teleoperation include launch
+    joystick_launch_dir = get_package_share_directory('teleop_twist_joy')
+    joystick_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource( joystick_launch_dir + "/launch/teleop-launch.py"),
+        launch_arguments={"joy_config": LaunchConfiguration("joy_config")}.items(),
+        condition=IfCondition(LaunchConfiguration("enable_joystick"))
+    )
+
+    # Joystick teleoperation include launch
+    robot_description_launch_dir = get_package_share_directory('swd_starter_kit_description')
+    robot_description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource( robot_description_launch_dir + "/launch/robot_description.launch.py")
+    )
+
+    return LaunchDescription(launch_args + [
+        diff_drive_controller,
+        lidar_launch,
+        joystick_launch,
+        robot_description_launch,
+    ])
